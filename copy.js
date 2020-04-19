9e23c4e9622b348aa5d77d8a67f586b8abe82399
apiraino <apiraino@users.noreply.github.com>
2020-04-19 23:48:32 +0200

    refactor query param checker and fix right click menu action

--- copy.js
+++ copy.js
@@ -15,7 +15,7 @@ function glob_to_regexp (pattern) {
                       .replace(/\*/g, '.*'));
 }
 
-// clean query params
+// remove tracking query params
 function maybe_clean (orig_url, urls, f) {
     var res = '';
     for (var idx = 0; idx < urls.length; idx++) {
@@ -28,7 +28,7 @@ function maybe_clean (orig_url, urls, f) {
     return (res != '' ? res : orig_url);
 }
 
-// clean URL
+// rewrite URL
 function maybe_clean_url (orig_url, urls, f) {
     var res = '';
     for (var idx = 0; idx < urls.length; idx++) {
@@ -41,21 +41,50 @@ function maybe_clean_url (orig_url, urls, f) {
     return (res != '' ? res : orig_url);
 }
 
+// pick one query param with a URL and redirect there
+function _redirect_to_query_param (orig_url) {
+    let res = '';
+    for (let i = 0; i < urls_to_param_mappers.length; i++) {
+        const item = urls_to_param_mappers[i];
+        const query_param = item.query_param ? item.query_param : 'url';
+        for (let idx = 0; idx < item.urls.length; idx++) {
+            const patt = glob_to_regexp(item.urls[idx]);
+            if (patt.test(orig_url)) {
+                res = redirect_to_query_param(query_param, orig_url)['redirectUrl'];
+                if (res != '') {
+                    break;
+                }
+            }
+        }
+    }
+    return (res != '' ? res : orig_url);
+}
+
 // this is only invoked on right-click menu action
 function cleaner_entrypoint (orig_link) {
 
-    // Remove tracking params form url
+    // first check: find a query param with the real URL and redirect there
+    let new_link = _redirect_to_query_param(orig_link);
+
+    // 2nd check: remove tracking query params
     // TODO: profile && improve perfs here
-    let new_link = maybe_clean(orig_link, all_urls, f_match_utm);
+    new_link = maybe_clean(new_link, all_urls, f_match_utm);
     new_link = maybe_clean(new_link, all_urls, f_match_fbclid);
     new_link = maybe_clean(new_link, aliexpress_regexp, f_match_all);
     new_link = maybe_clean(new_link, fbcontent_regexp, f_match_fbcontent);
     new_link = maybe_clean(new_link, instagram_regexp, f_match_igshid);
 
-    // clean URL
-    new_link = maybe_clean_url(new_link, ["amazon"], clean_amazon);
+    // 3rd check: (optional) clean Amazon URL
+    if (settings['clean_amp_links'] === true) {
+        new_link = maybe_clean_url(new_link, ["amazon"], clean_amazon);
+    }
 
-    console.debug("[copytoclipboard] link cleaned: " + new_link);
+    // 4th check: (optional) redirect reddit URL
+    if (settings['redirect_reddit_nojs'] === true) {
+        new_link = new_link.replace('www', 'old');
+    }
+
+    // console.debug("[copytoclipboard] link cleaned: " + new_link);
     return new_link;
 }
 
@@ -86,7 +115,7 @@ function copyToClipboard(obj) {
 */
 
 function oncopy(event) {
-    console.debug("[oncopy] " + global_link_text);
+    // console.debug("[oncopy] " + global_link_text);
     if (global_link_text === "") {
         return;
     }
